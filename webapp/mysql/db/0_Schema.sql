bc05428980d2337d53694c2cb5d3b82179010498
ryunosuke121 <ryunosuke.ito.121@gmail.com>
2024-10-21 15:01:45 +0900

    スキーマに入れる

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -17,7 +17,8 @@ CREATE TABLE isuumo.estate
     door_height INTEGER             NOT NULL,
     door_width  INTEGER             NOT NULL,
     features    VARCHAR(64)         NOT NULL,
-    popularity  INTEGER             NOT NULL
+    popularity  INTEGER             NOT NULL,
+    popularity_minus INTEGER GENERATED ALWAYS AS (-popularity) STORED 
 );
 
 CREATE TABLE isuumo.chair
